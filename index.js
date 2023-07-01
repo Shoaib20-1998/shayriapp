62e1fb880e01f54767b45d138f25ffcf1659b535
Your Name <sm920149@gmail.com>
2023-07-01 10:32:07 +0530

    data pushed

--- index.js
+++ index.js
@@ -7,27 +7,28 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-app.post('/shayri', async (req, res) => {
-    try {
-      const { message } = req.body; // Assuming you're receiving the user's message in the request body
-      // Make the API request to ChatGPT
-      const response = await axios.post('https://api.openai.com/v1/chat/completions', {
-        model: 'gpt-3.5-turbo',
-        messages: [{ role: 'system', content: 'You are a helpful assistant.' }, { role: 'user', content: message }],
-      }, {
-        headers: {
-          'Authorization': `Bearer ${process.env.apiKey}`,
-          'Content-Type': 'application/json'
-        }
-      });
-  
-      // Return the API response to the client
-      res.json(response.data);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'An error occurred' });
-    }
+app.post('/shayari', async (req, res) => {
+  const { message } = req.body;
+
+  // Call the ChatGPT API here using Axios
+  // Replace 'YOUR_API_KEY' with your actual ChatGPT API key
+  try {
+    const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+    model: 'gpt-3.5-turbo',
+    messages: [{ role: 'system', content: 'You are a helpful assistant.' }, { role: 'user', content: message }],
+  }, {
+    headers: {
+      'Authorization': `Bearer ${process.env.apiKey}`,
+      'Content-Type': 'application/json',
+    },
   });
+
+  const reply = response.data.choices[0].message.content;
+  res.json({ reply });
+  } catch (error) {
+    res.send(error)
+  }
+});
 
   app.get('/check',async(req,res)=>{
 
