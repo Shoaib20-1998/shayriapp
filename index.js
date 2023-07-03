bd1f85b9ba37c358b17caa363ea538641fbd4bcd
Your Name <sm920149@gmail.com>
2023-07-03 17:00:31 +0530

    convertor

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-// app.get('/shayari', async (req, res) => {
+// app.get('/codeconvert', async (req, res) => {
 //   try {
 //     const keyword = req.query.keyword;
 //     const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', {
@@ -62,8 +62,42 @@ app.post('/shayari', async (req, res) => {
       
     }
 
-  })
+})
 
+
+app.post('/convertor', async (req, res) => {
+  const { message } = req.body;
+  // Call the ChatGPT API here using Axios
+  // Replace 'YOUR_API_KEY' with your actual ChatGPT API key
+  try {
+    const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+    model: 'gpt-3.5-turbo',
+    messages: [{ role: 'system', content: 'You are a helpful assistant.' }, { role: 'user', content: message }],
+  }, {
+    headers: {
+      'Authorization': `Bearer ${process.env.apiKey}`,
+      'Content-Type': 'application/json',
+    },
+  });
+  const reply = response.data.choices[0].message.content;
+  console.log(reply)
+  res.json({ reply });
+  } catch (error) {
+    res.send(error)
+
+  }
+});
+
+  app.get('/check',async(req,res)=>{
+
+    try {
+      res.send("hy")
+    } catch (error) {
+      res.send(error)
+      
+    }
+
+})
 
 app.listen(8080,()=>{
     console.log("running")
