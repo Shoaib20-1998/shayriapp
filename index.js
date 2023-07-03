0096ecf4bed1b303dac5bfca9387486d24517567
Your Name <sm920149@gmail.com>
2023-07-03 14:08:43 +0530

    data

--- index.js
+++ index.js
@@ -7,51 +7,51 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-app.get('/shayari', async (req, res) => {
-  try {
-    const keyword = req.query.keyword;
-    const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', {
-      prompt: `Shayari about ${keyword}`,
-      max_tokens: 100,
-      temperature: 0.7,
-      n: 1
-    }, {
-      headers: {
-        'Authorization': `Bearer ${process.env.apiKey}`,
-        'Content-Type': 'application/json'
-      }
-    });
-
-    const shayari = response.data.choices[0].text.trim();
-    res.json({ shayari });
-  } catch (error) {
-    console.error('Error:', error.response.data);
-    res.status(500).json({ error: 'Something went wrong' });
-  }
-});
-
-// app.post('/shayari', async (req, res) => {
-//   const { message } = req.body;
-//   // Call the ChatGPT API here using Axios
-//   // Replace 'YOUR_API_KEY' with your actual ChatGPT API key
+// app.get('/shayari', async (req, res) => {
 //   try {
-//     const response = await axios.post('https://api.openai.com/v1/chat/completions', {
-//     model: 'gpt-3.5-turbo',
-//     messages: [{ role: 'system', content: 'You are a helpful assistant.' }, { role: 'user', content: message }],
-//   }, {
-//     headers: {
-//       'Authorization': `Bearer ${process.env.apiKey}`,
-//       'Content-Type': 'application/json',
-//     },
-//   });
-//   const reply = response.data.choices[0].message.content;
-//   console.log(reply)
-//   res.json({ reply });
-//   } catch (error) {
-//     res.send(error)
+//     const keyword = req.query.keyword;
+//     const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', {
+//       prompt: `Shayari about ${keyword}`,
+//       max_tokens: 100,
+//       temperature: 0.7,
+//       n: 1
+//     }, {
+//       headers: {
+//         'Authorization': `Bearer ${process.env.apiKey}`,
+//         'Content-Type': 'application/json'
+//       }
+//     });
 
+//     const shayari = response.data.choices[0].text.trim();
+//     res.json({ shayari });
+//   } catch (error) {
+//     console.error('Error:', error.response.data);
+//     res.status(500).json({ error: 'Something went wrong' });
 //   }
 // });
+
+app.post('/shayari', async (req, res) => {
+  const { message } = req.body;
+  // Call the ChatGPT API here using Axios
+  // Replace 'YOUR_API_KEY' with your actual ChatGPT API key
+  try {
+    const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+    model: 'gpt-3.5-turbo',
+    messages: [{ role: 'system', content: 'You are a helpful assistant.' }, { role: 'user', content: message }],
+  }, {
+    headers: {
+      'Authorization': `Bearer ${process.env.apiKey}`,
+      'Content-Type': 'application/json',
+    },
+  });
+  const reply = response.data.choices[0].message.content;
+  console.log(reply)
+  res.json({ reply });
+  } catch (error) {
+    res.send(error)
+
+  }
+});
 
   app.get('/check',async(req,res)=>{
 
