c73bbb634616261d7a615b12d5d6a9b33107e1d9
Your Name <sm920149@gmail.com>
2023-07-03 17:28:27 +0530

    code convertor

--- index.js
+++ index.js
@@ -88,16 +88,55 @@ app.post('/convertor', async (req, res) => {
   }
 });
 
-  app.get('/check',async(req,res)=>{
 
-    try {
-      res.send("hy")
-    } catch (error) {
-      res.send(error)
-      
-    }
+app.post('/debuger', async (req, res) => {
+  const { message } = req.body;
+  // Call the ChatGPT API here using Axios
+  // Replace 'YOUR_API_KEY' with your actual ChatGPT API key
+  try {
+    const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+    model: 'gpt-3.5-turbo',
+    messages: [{ role: 'system', content: 'You are a helpful assistant.' }, { role: 'user', content: message }],
+  }, {
+    headers: {
+      'Authorization': `Bearer ${process.env.apiKey}`,
+      'Content-Type': 'application/json',
+    },
+  });
+  const reply = response.data.choices[0].message.content;
+  console.log(reply)
+  res.json({ reply });
+  } catch (error) {
+    res.send(error)
 
-})
+  }
+});
+
+app.post('/check', async (req, res) => {
+  const { message } = req.body;
+  // Call the ChatGPT API here using Axios
+  // Replace 'YOUR_API_KEY' with your actual ChatGPT API key
+  try {
+    const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+    model: 'gpt-3.5-turbo',
+    messages: [{ role: 'system', content: 'You are a helpful assistant.' }, { role: 'user', content: message }],
+  }, {
+    headers: {
+      'Authorization': `Bearer ${process.env.apiKey}`,
+      'Content-Type': 'application/json',
+    },
+  });
+  const reply = response.data.choices[0].message.content;
+  console.log(reply)
+  res.json({ reply });
+  } catch (error) {
+    res.send(error)
+
+  }
+});
+
+
+  
 
 app.listen(8080,()=>{
     console.log("running")
